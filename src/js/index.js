41ea20947e59e74c37a068cad32ea74a96a8b243
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-13 11:21:05 +0000

    counter

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import reloj from "./../img/reloj.png";
 
 //import your own components
 import Home from "./component/home.jsx";
 
+function SimpleCounter(){
+    return (<div className="bigCounter">
+        <div className="calendar">
+            <img reloj></i>
+        </div>
+        <div className="four">0</div>
+        <div className="three">0</div>
+        <div className="two">0</div>
+        <div className="one">0</div>
+    </div>);
+}
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+
+
